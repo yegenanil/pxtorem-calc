cd6c460a2b0ada1d0039ec4e5b3c2a242e05006e
anil yegen <ygnanil@gmail.com>
2022-05-19 16:57:50 +0300

    add: algorithm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const pxInput = useRef();
+  const remInput = useRef();
+
+  const handlePxChange = (value) => {
+    pxInput.current.value = value;
+    if (value === '') {
+      remInput.current.value = '';
+      return;
+    }
+
+    const numberValue = parseInt(value);
+    if (numberValue === 0) {
+      remInput.current.value = '0';
+    } else if (numberValue > 0) {
+      remInput.current.value = numberValue / 16;
+    }
+  }
+
+  const handleRemChange = (value) => {
+    remInput.current.value = value;
+    if (value === '') {
+      pxInput.current.value = '';
+      return;
+    }
+
+    const numberValue = parseInt(value);
+    if (numberValue === 0) {
+      pxInput.current.value = '0';
+    } else if (numberValue > 0) {
+      pxInput.current.value = numberValue * 16;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={(e) => e.preventDefault()} className="form-container">
+        <div className='px-container'>
+          <h1>Pixels</h1>
+          <input type="text" ref={pxInput} defaultValue={''} onChange={(e) => handlePxChange(e.target.value)} />
+        </div>
+        <div className='rem-container'>
+          <h1>REM</h1>
+          <input type="text" ref={remInput} defaultValue={''} onChange={(e) => handleRemChange(e.target.value)} />
+        </div>
+      </form>
+    </>
   );
 }
 
